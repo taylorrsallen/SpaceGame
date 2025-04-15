1b1626c05d5c2590f0762a374df8a292384fd0ad
Lone Walker Studios <66659302+mickTrumble@users.noreply.github.com>
2025-04-15 02:36:55 -0500

    additions

--- Assets/Systems/Npcs/PlasmaBall.cs
+++ Assets/Systems/Npcs/PlasmaBall.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 using NPCs.Base;
 public class PlasmaBall : NPC
 {
+    [SerializeField] private GameEffectData PlasmaImpact;
     protected override void OnDeath()
     {
-
+        PlasmaImpact.Execute(new GameEffectArgs(this.gameObject, null, transform.position));
+        Destroy(gameObject);
     }
 }
