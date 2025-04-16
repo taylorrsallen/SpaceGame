6b05ab7f2ade69d3fe1c707151b8d91b7186f285
Lone Walker Studios <66659302+mickTrumble@users.noreply.github.com>
2025-04-16 16:05:10 -0500

    Elonzo muck

--- Assets/Systems/Npcs/SpaceSpeksAiType.cs
+++ Assets/Systems/Npcs/SpaceSpeksAiType.cs
@@ -6,26 +6,39 @@ public class SpaceSpeksAiType : AiType
     private Rigidbody _rb;
     private BoxCollider _boxCollider;
 
+    private float _timeLeftTillBlowup = 0;
+
+
     protected override void OnInit()
     {
         _boxCollider = gameObject.AddComponent<BoxCollider>();
         _rb = gameObject.AddComponent<Rigidbody>();
-        _boxCollider.size = new Vector3(4, 10, 4);
+        _boxCollider.size = new Vector3(4, 14, 2);
         _rb.useGravity = true;
 
         _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
-        _rb.mass = 2f;
+        _rb.mass = 122;
         _rb.linearDamping = 0.5f;
-
-        
+        enabled = false;
+        Invoke("S_FlyWhenRight", Random.Range(3, 12));
     }
     public void Update()
     {
-        _rb.AddForce(0, 330 * Time.deltaTime, 0);
-        CheckForKillDistance();
+        _timeLeftTillBlowup -= Time.deltaTime;
+
+        _rb.AddRelativeForce(Vector3.up * 85000 * Time.deltaTime);
+
+    }
+
+    protected void S_FlyWhenRight()
+    {
+        enabled = true;
     }
     public void OnCollisionEnter(Collision collision)
     {
-        _npcRoot.Kill();
+        if(Mathf.Abs(collision.impulse.magnitude) >= 800)
+        {
+            _npcRoot.TakeDamage(0.01f * collision.impulse.magnitude);
+        }
     }
 }
