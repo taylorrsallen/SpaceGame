e07d90216dd514326ed421bfb838c619dec461b6
Lone Walker Studios <66659302+mickTrumble@users.noreply.github.com>
2025-04-15 19:22:11 -0500

    UFOUPDATE

--- Assets/Systems/Npcs/PlasmaBallAiType.cs
+++ Assets/Systems/Npcs/PlasmaBallAiType.cs
@@ -4,19 +4,23 @@ public class PlasmaBallAiType : AiType
 {
     private Rigidbody _rb;
     private BoxCollider _boxCollider;
-
+    [SerializeField] private float speed = 35;
     protected override void OnInit()
     {
         _boxCollider = gameObject.AddComponent<BoxCollider>();
         _rb = gameObject.AddComponent<Rigidbody>();
-        _boxCollider.size = new Vector3(0.5f, 0.5f, 0.5f);
+        _boxCollider.size = new Vector3(2, 2, 2);
         _rb.useGravity = false;
 
         _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
         _rb.mass = 0.01f;
         _rb.linearDamping = 0;
 
-        _rb.AddRelativeForce(Vector3.forward * 2);
+        Invoke("Thingy", 0.01f);
+    }
+    private void Thingy()
+    {
+        _rb.linearVelocity = transform.forward * speed;
     }
     public void Update()
     {
